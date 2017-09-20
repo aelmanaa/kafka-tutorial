4d00a66f3d1e8c1938db43883cee5ec83a4dc7d8
Amine El Manaa <aelmanaa@gmail.com>
2017-09-20 18:11:46 +0900

    remove APptest files generated by maven plugin

---
+++ kafka-consumer/src/main/java/com/sbibits/ptrc/tutorials/kafka/ConsumerExample.java
@@ -0,0 +1,63 @@
+package com.sbibits.ptrc.tutorials.kafka;
+
+import java.util.Collections;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class ConsumerExample {
+  private final static String TOPIC = "my-example-topic";
+  private final static String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
+
+
+  private static Consumer<Long, String> createConsumer() {
+    final Properties props = new Properties();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+    // Create the consumer using props.
+    final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+
+    // Subscribe to the topic.
+    consumer.subscribe(Collections.singletonList(TOPIC));
+    return consumer;
+  }
+
+  static void runConsumer() throws InterruptedException {
+    final Consumer<Long, String> consumer = createConsumer();
+
+    final int giveUp = 100;
+    int noRecordsCount = 0;
+
+    while (true) {
+      final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+
+      if (consumerRecords.count() == 0) {
+        noRecordsCount++;
+        if (noRecordsCount > giveUp)
+          break;
+        else
+          continue;
+      }
+
+      consumerRecords.forEach(record -> {
+        System.out.printf("Consumer Record:(%d, %s, %d, %d)\n", record.key(), record.value(),
+            record.partition(), record.offset());
+      });
+
+      consumer.commitAsync();
+    }
+    consumer.close();
+    System.out.println("DONE");
+  }
+
+  public static void main(String... args) throws Exception {
+    runConsumer();
+  }
+}
--- kafka-producer/src/main/java/com/sbibits/ptrc/tutorials/kafka/ProducerExample.java
+++ kafka-producer/src/main/java/com/sbibits/ptrc/tutorials/kafka/ProducerExample.java
@@ -3,6 +3,8 @@ package com.sbibits.ptrc.tutorials.kafka;
 
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -51,11 +53,41 @@ public class ProducerExample {
     }
   }
 
+  static void runProducerAsync(final int sendMessageCount) throws Exception {
+    final Producer<Long, String> producer = createProducer();
+    long time = System.currentTimeMillis();
+    final CountDownLatch countDownLatch = new CountDownLatch(sendMessageCount);
+
+    try {
+      for (long index = time; index < time + sendMessageCount; index++) {
+        final ProducerRecord<Long, String> record =
+            new ProducerRecord<>(TOPIC, index, "Hello Mom " + index);
+        producer.send(record, (metadata, exception) -> {
+          long elapsedTime = System.currentTimeMillis() - time;
+          if (metadata != null) {
+            System.out.printf(
+                "sent record(key=%s value=%s) " + "meta(partition=%d, offset=%d) time=%d\n",
+                record.key(), record.value(), metadata.partition(), metadata.offset(), elapsedTime);
+          } else {
+            exception.printStackTrace();
+          }
+          countDownLatch.countDown();
+        });
+      }
+      countDownLatch.await(25, TimeUnit.SECONDS);
+    } finally {
+      producer.flush();
+      producer.close();
+    }
+  }
+
   public static void main(String... args) throws Exception {
     if (args.length == 0) {
-      runProducer(5);
+      runProducer(25);
+      //runProducerAsync(5);
     } else {
-      runProducer(Integer.parseInt(args[0]));
+       runProducer(Integer.parseInt(args[0]));
+      //runProducerAsync(Integer.parseInt(args[0]));
     }
   }
 }
